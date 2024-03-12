11fedd4ac4f29c490a8e4f0a383ceb4b8110f1ff
Song Yu Jeong <udeanglvup@gmail.com>
2024-03-13 04:09:45 +0900

    feat:시작 페이지 컴포넌트 구현, 스타일 지정

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import styled from "styled-components";
+import StartPage from "./pages/StartPage";
 
 function App() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Router>
+        <Routes>
+          <Route path="/" element={<StartPage />} />
+        </Routes>
+      </Router>
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/pages/StartPage.jsx
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+import background from "../../public/images/asset/start_bg.png";
+import logo from "../../public/images/asset/logo.png";
+
+const StartPage = () => {
+  return (
+    <Container>
+      <Logo src={logo} alt="Logo" />
+      <SubTitle>나랑 닮은 주술회전 캐릭터는?</SubTitle>
+      <Button>START</Button>
+    </Container>
+  );
+};
+export default StartPage;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  height: 100vh;
+  background-image: url(${background});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center end;
+  @media (max-width: 768px) {
+    background-position: center;
+  }
+`;
+
+const Logo = styled.img`
+  width: 100%;
+  @media (max-width: 768px) {
+    margin-top: -20px;
+  }
+`;
+
+const SubTitle = styled.div`
+  font-weight: bold;
+  @media (max-width: 768px) {
+    margin-top: -1rem;
+  }
+`;
+
+const Button = styled.div`
+  background-color: #2f3648;
+  color: white;
+  padding: 0.5rem;
+  margin-top: 2rem;
+  border-radius: 0.5rem;
+  transition: background-color 0.3s;
+  cursor: pointer;
+  &:hover {
+    background-color: #cbcbcb;
+  }
+  @media (max-width: 768px) {
+    margin-top: 1rem;
+  }
+`;
