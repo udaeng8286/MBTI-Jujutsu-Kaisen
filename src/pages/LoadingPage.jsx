243728a4186b51862b1e12cd7fd371204cbb05ef
Song Yu Jeong <udeanglvup@gmail.com>
2024-03-14 02:18:16 +0900

    feat: 백그라운드 이미지, 분석중 애니메이션 구현

--- src/pages/LoadingPage.jsx
+++ src/pages/LoadingPage.jsx
@@ -1,4 +1,35 @@
+import styled from "styled-components";
+import background from "../../public/images/asset/loading_bg.png";
+import LoadingBar from "../components/LoadingBar";
+
 const LoadingPage = () => {
-  return <div>로딩페이지</div>;
+  return (
+    <Container>
+      <Text>분석중!</Text>
+      <LoadingBar />
+    </Container>
+  );
 };
 export default LoadingPage;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  height: 100vh;
+  background-image: url(${background});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center end;
+  @media (max-width: 768px) {
+    background-position: center;
+  }
+`;
+
+const Text = styled.div`
+  font-weight: bold;
+  @media (max-width: 768px) {
+    margin-top: -1rem;
+  }
+`;
