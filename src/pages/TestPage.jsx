70cdd5b7a81587f4984d1f3234890104b451f071
Song Yu Jeong <udeanglvup@gmail.com>
2024-03-15 14:30:53 +0900

    feat:테스트 페이지 배경이미지 설정

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,4 +1,22 @@
+import styled from "styled-components";
+import background from "../../public/images/asset/test_bg.png";
+
 const TestPage = () => {
-  return <div>testpage</div>;
+  return <Container>testpage</Container>;
 };
 export default TestPage;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  height: 100vh;
+  background-image: url(${background});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center end;
+  @media (max-width: 768px) {
+    background-position: center;
+  }
+`;
