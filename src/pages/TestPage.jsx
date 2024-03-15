f3bb7d4ae98b22099abfe16e89d0f5b9ae501ebf
Song Yu Jeong <udeanglvup@gmail.com>
2024-03-15 16:43:52 +0900

    feat:테스트 페이지 기능 구현

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,15 +1,70 @@
 import styled from "styled-components";
 import background from "../../public/images/asset/test_bg.png";
-// import questionData from "../data/question.json";
+import questionData from "../data/question.json";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const TestPage = () => {
+  const navigate = useNavigate();
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [typeResult, setTypeResult] = useState({
+    E: 0,
+    I: 0,
+    S: 0,
+    N: 0,
+    T: 0,
+    F: 0,
+    P: 0,
+    J: 0,
+  });
+
+  //선택된 답변들로 최종유형 계산 함수
+  const calculateType = () => {
+    const type =
+      (typeResult.E > typeResult.I ? "E" : "I") +
+      (typeResult.N > typeResult.S ? "N" : "S") +
+      (typeResult.T > typeResult.F ? "T" : "F") +
+      (typeResult.J > typeResult.P ? "J" : "P");
+    console.log("Calculated MBTI Type:", type);
+    return type;
+  };
+
+  //사용자가 답변 선택할때 호출되는 함수
+  const handleOptionSelect = (type) => {
+    //선택된 답변에 따라 결과 업데이트
+    setTypeResult((prevResult) => {
+      return {
+        ...prevResult,
+        [type]: prevResult[type] + 1,
+      };
+    });
+    //다음 질문으로 이동 또는 결과 페이지로 이동
+    if (currentIndex < questionData.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    } else {
+      const mbtiType = calculateType();
+      const encryptedMbtiType = mbtiType;
+      navigate(`/result?mbtiType=${encodeURIComponent(encryptedMbtiType)}`);
+    }
+  };
+  console.log("결과", typeResult);
+
   return (
     <Container>
-      <QuestionContainer></QuestionContainer>
-      <AnswerContainer>
-        <Answer1></Answer1>
-        <Answer2></Answer2>
-      </AnswerContainer>
+      <TestContainer>
+        <QuestionWrapper>
+          <Question>{questionData[currentIndex].question}</Question>
+        </QuestionWrapper>
+        <AnswerWrap>
+          {Object.entries(questionData[currentIndex].answers).map(
+            ([key, value]) => (
+              <Answer key={key} onClick={() => handleOptionSelect(key)}>
+                {value}
+              </Answer>
+            )
+          )}
+        </AnswerWrap>
+      </TestContainer>
     </Container>
   );
 };
@@ -31,29 +86,30 @@ const Container = styled.div`
   }
 `;
 
-const QuestionContainer = styled.div`
+const TestContainer = styled.div`
   width: 400px; //임시. 수정 필요
-  height: 240px; //임시. 수정 필요
-  border-radius: 10px;
+`;
+
+const QuestionWrapper = styled.div`
+  width: 100%;
   background-color: #2f3648;
-`;
-
-const AnswerContainer = styled.div`
-  margin-top: 2rem; //임시. 수정 필요
-  width: 400px; //임시. 수정 필요
-`;
-
-const Answer1 = styled.div`
-  width: 100%; //임시. 수정 필요
-  height: 120px; //임시. 수정 필요
+  padding: 1rem;
   border-radius: 10px;
-  background-color: #ffffff;
+`;
+const Question = styled.text`
+  color: white;
 `;
 
-const Answer2 = styled.div`
-  margin-top: 2rem; //임시. 수정 필요
-  width: 100%; //임시. 수정 필요
-  height: 120px; //임시. 수정 필요
+const AnswerWrap = styled.div`
+  width: 100%;
+  margin-top: 1rem;
+`;
+
+const Answer = styled.div`
+  margin-top: 1rem;
+  padding: 1rem;
+  width: 100%;
+  background-color: white;
+  color: #2f3648;
   border-radius: 10px;
-  background-color: #ffffff;
 `;
