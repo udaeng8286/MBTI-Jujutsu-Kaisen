cb113d081c56e172c3a15f630c95aeef344461b5
Song Yu Jeong <udeanglvup@gmail.com>
2024-03-18 17:39:19 +0900

    feat:결과 페이지 파일 추가

---
+++ src/components/ShareButton.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import kakaoIcon from "../../public/images/asset/kakao.png";
+import twitterIcon from "../../public/images/asset/twitter.png";
+import linkIcon from "../../public/images/asset/link.png";
+
+const ShareButton = () => {
+  return <Container></Container>;
+};
+export default ShareButton;
+
+const Container = styled.div``;
---
+++ src/pages/ResultPage.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import React from "react";
+import ShareButton from "../components/ShareButton";
+const ResultPage = () => {
+  return (
+    <Container>
+      <ShareButton />
+    </Container>
+  );
+};
+export default ResultPage;
+
+const Container = styled.div``;
