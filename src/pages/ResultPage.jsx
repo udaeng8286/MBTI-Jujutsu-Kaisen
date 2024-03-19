f809cc1ca2a9a535e9b624ca4dc0f399fd7ead01
Song Yu Jeong <udeanglvup@gmail.com>
2024-03-19 22:42:58 +0900

    feat:결과 페이지 배경이미지 설정

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,16 +1,24 @@
 import styled from "styled-components";
 import ShareButton from "../components/ShareButton";
+import background from "../../public/images/asset/result_bg.png";
 
 const ResultPage = () => {
-  return (
-    <Container>
-      <ShareButton />
-    </Container>
-  );
+  return <Container></Container>;
 };
 export default ResultPage;
 
 const Container = styled.div`
-  width: 100%;
+  display: flex;
+  flex-direction: column;
+  /* justify-content: center; */
+  align-items: center;
+  margin: 0 auto;
   height: 100vh;
+  background-image: url(${background});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center end;
+  @media (max-width: 768px) {
+    background-position: center;
+  }
 `;
