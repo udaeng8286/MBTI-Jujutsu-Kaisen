49e3f640868073f5cdae8827c0f6143e31f636b1
Song Yu Jeong <udeanglvup@gmail.com>
2024-03-20 03:11:38 +0900

    feat: 결과 페이지 기능 구현

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,15 +1,32 @@
 import styled from "styled-components";
 import ShareButton from "../components/ShareButton";
 import background from "../../public/images/asset/result_bg.png";
+import characterData from "../data/results.json";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const ResultPage = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const mbtiType = new URLSearchParams(location.search).get("mbtiType");
+
+  const characterInfo = characterData.find(
+    (character) => character.type === mbtiType
+  );
+
+  const handleReplay = () => {
+    navigate("/");
+  };
+
   return (
     <Container>
-      <CharacterImg></CharacterImg>
-      <InfoContainer></InfoContainer>
+      <CharacterImg src={characterInfo.characterImg} />
+      <InfoContainer>
+        <Character>{characterInfo.character}</Character>
+        <Info>{characterInfo.description}</Info>
+      </InfoContainer>
       <BottomSection>
         <ShareButton />
-        <ReplayButton>다시하기</ReplayButton>
+        <ReplayButton onClick={handleReplay}>다시하기</ReplayButton>
       </BottomSection>
     </Container>
   );
@@ -19,7 +36,7 @@ export default ResultPage;
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  /* justify-content: center; */
+  justify-content: center;
   align-items: center;
   margin: 0 auto;
   height: 100vh;
@@ -32,13 +49,29 @@ const Container = styled.div`
   }
 `;
 
-const CharacterImg = styled.div``;
+const CharacterImg = styled.img`
+  width: 300px;
+  margin-bottom: 1.5rem;
+`;
 
 const InfoContainer = styled.div`
   background-color: white;
   width: 90%;
-  height: 240px;
+  height: auto;
   border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 1rem;
+  font-weight: bold;
+`;
+
+const Character = styled.div`
+  font-size: 24px;
+`;
+
+const Info = styled.div`
+  margin-top: 1rem;
 `;
 
 const BottomSection = styled.div`
