43a41e3ead33023d833b12d3f571e00a78c23914
Song Yu Jeong <udeanglvup@gmail.com>
2024-03-20 19:51:13 +0900

    feat : 주석 제거

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,12 @@ import StartPage from "./pages/StartPage";
 import TestPage from "./pages/TestPage";
 import ResultPage from "./pages/ResultPage";
 import BackgroundMusic from "./components/BackgroundMusic";
-// import RouteChangeTracker from "./components/RouteChangeTracker";
-import ReactGA from "react-ga";
-
-// const gaTrackingId = import.meta.env.VITE_GOOGLE_ANALYTICS_ID;
-// ReactGA.initialize(gaTrackingId, { debug: true });
-// ReactGA.pageview(window.location.pathname);
 
 function App() {
   return (
     <Container>
       <BackgroundMusic />
       <Router>
-        {/* <RouteChangeTracker /> */}
         <Routes>
           <Route path="/" element={<StartPage />} />
           <Route path="/test" element={<TestPage />} />
