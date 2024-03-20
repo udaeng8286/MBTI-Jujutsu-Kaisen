3c75e01b40adc95da2436f4e6b3b7ff23bd6d2bc
Song Yu Jeong <udeanglvup@gmail.com>
2024-03-20 11:32:04 +0900

    feat: on, off에 따른 배경음악 아이콘 변동 및 음악 재생 기능 구현

--- src/components/BackgroundMusic.jsx
+++ src/components/BackgroundMusic.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import styled from "styled-components";
+import { FaVolumeUp, FaVolumeMute } from "react-icons/fa";
 
 const BackgroundMusic = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audio = new Audio(`/bgm.mp3`);
+
+  const toggleMusic = () => {
+    if (isPlaying) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
   return (
     <Container>
-      <Button></Button>
+      <Button onClick={toggleMusic}>
+        {isPlaying ? <FaVolumeUp /> : <FaVolumeMute />}
+      </Button>
     </Container>
   );
 };
