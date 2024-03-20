0c1d55f620d6d3f9c386ad52a2ed44f595d18af9
Song Yu Jeong <udeanglvup@gmail.com>
2024-03-20 11:25:46 +0900

    feat: react-icons 설치 및 BGM 컴포넌트 생성

---
+++ src/components/BackgroundMusic.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const BackgroundMusic = () => {
+  return (
+    <Container>
+      <Button></Button>
+    </Container>
+  );
+};
+
+export default BackgroundMusic;
+
+const Container = styled.div``;
+
+const Button = styled.div``;
